c84a2f67a6810cf3eaf5af37618abbcf52acea42
samoolean <samuel.choisi1@gmail.com>
2024-10-12 20:56:36 +0200

    first commit
     1

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,12 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "La Galerie", "/pages/galerie.html"),
+    new Route("/signin", "Signin", "/pages/auth/signin.html"),
+    new Route("/signup", "Signup", "/pages/auth/signup.html", "/js/auth/signup.js"),
+    new Route("/editpassword", "editpassword", "/pages/auth/editpassword.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
-export const websiteName = "Quai Antique";
+export const websiteName = "Tim la princesse";
